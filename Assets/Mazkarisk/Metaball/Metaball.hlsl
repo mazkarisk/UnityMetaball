b0746c6143b7d2ba417c4ca1587d9e6b16139571
mazkarisk <108807611+mazkarisk@users.noreply.github.com>
2025-09-21 22:32:54 +0900

    基本的なメタボール表現が可能になるよう機能追加

---
+++ Assets/Mazkarisk/Metaball/Metaball.hlsl
@@ -0,0 +1,151 @@
+#ifndef METABALL_INCLUDED
+#define METABALL_INCLUDED
+
+/********/
+/* 定数 */
+/********/
+
+#define MAX_SPHERE_COUNT 1024 // 球の最大数
+
+
+/******************************/
+/* スクリプトから渡される情報 */
+/******************************/
+
+int _SphereCount; // 処理対象となる球の個数
+float _SmoothWidth; // 滑らかなmin関数の滑らか度合い
+int _MaxMarchCount; // レイの最大行進回数
+float _MaxMarchDistance; // レイの最大行進距離(スクリプトから"カメラから最も遠い球の向こう側までの距離"を設定することを想定)
+float _HitThreshold; // レイの衝突判定閾値(レイの先端とメタボールの距離がこの値以下になったら"ヒット"したと判定する)
+int _DebugViewEnable; // デバッグ表示(0:しない, 1:更新回数を表示)
+StructuredBuffer<float4> _SpheresBuffer; // 球の位置情報(x:x座標, y:y座標, z:z座標, w:半径)
+
+
+/**************************/
+/* レイマーチング関連処理 */
+/**************************/
+
+// 滑らかなmin関数
+float smoothMin(float a, float b, float w) {
+	const float x = (b - a) / w;
+	return x <= -1 ? b : x >= 1 ? a : b - w * 0.25 * (x + 1) * (x + 1);
+}
+
+// 球の距離関数
+float sphereDistanceFunction(float4 sphere, float3 pos) {
+	return length(sphere.xyz - pos) - sphere.w;
+}
+
+// 球との最短距離を返す
+float getDistance(float3 pos) {
+	float distance = _MaxMarchDistance; // 初期値として十分に遠い距離を設定
+	for (int i = 0; i < _SphereCount; i++) {
+		distance = smoothMin(distance, sphereDistanceFunction(_SpheresBuffer[i], pos), _SmoothWidth);
+	}
+	return distance;
+}
+
+// 指定位置の法線を偏微分により取得
+float3 getNormal(const float3 pos) {
+	const float delta = _HitThreshold;
+	const float x = getDistance(pos + float3(delta, 0, 0)) - getDistance(pos + float3(-delta, 0, 0));
+	const float y = getDistance(pos + float3(0, delta, 0)) - getDistance(pos + float3(0, -delta, 0));
+	const float z = getDistance(pos + float3(0, 0, delta)) - getDistance(pos + float3(0, 0, -delta));
+	return normalize(float3(x, y, z));
+}
+
+
+/****************************************/
+/* Shader Graphからのエントリーポイント */
+/****************************************/
+
+void Metaball_float(
+		float3 CameraPosition,
+		float3 CameraDirection,
+		float3 FragmentPosition,
+		float SceneDepth,
+		float4x4 ViewMatrix,
+		out float3 BaseColor,
+		out float Alpha,
+		out float3 Normal,
+		out float DepthOffset) {
+
+	const float3 rayDir = normalize(FragmentPosition - CameraPosition); // レイの進行方向
+	
+	// 深度補正値の計算
+	const float sceneDepthDistanceRate = 1 / dot(rayDir, CameraDirection);
+	const float sceneDepthDistance = SceneDepth * sceneDepthDistanceRate;
+
+	// デフォルト値の設定
+	BaseColor = float3(1, 1, 1); // 色は白
+	Alpha = 1; // アルファクリッピングは無し
+	Normal = float3(0, 1, 0); // 法線は上向き
+	DepthOffset = 0; // 深度オフセットは無し
+
+	// レイマーチング準備
+	float3 pos = CameraPosition; // レイの座標(カメラのワールド座標から開始)
+	float totalDistance = 0; // 合計行進距離
+	bool hit = false; // レイがヒットしたかどうかのフラグ、ヒットしなければfalseのまま
+	int marchCount = 0;
+	
+	// レイマーチング開始
+	for (marchCount = 1; marchCount <= _MaxMarchCount; marchCount++) {
+		const float distance = getDistance(pos);
+
+		// 距離が閾値以下になったらヒットしたとみなして処理終了
+		if (distance <= _HitThreshold) {
+			hit = true;
+			break;
+		}
+
+		// 合計行進距離が描画済み深度に到達したら強制終了
+		if (totalDistance >= sceneDepthDistance) {
+			if (_DebugViewEnable) {
+				const float marchRate = (float)marchCount / _MaxMarchCount;
+				BaseColor = float3(0, 0, marchRate); // 青色で回数を表示
+			} else {
+				Alpha = 0; // アルファを0にしてクリップさせる
+			}
+			return;
+		}
+
+		// 合計行進距離が最大描画距離に到達したら強制終了
+		if (totalDistance >= _MaxMarchDistance) {
+			if (_DebugViewEnable) {
+				const float marchRate = (float)marchCount / _MaxMarchCount;
+				BaseColor = float3(0, marchRate, marchRate); // 水色で回数を表示
+			} else {
+				Alpha = 0; // アルファを0にしてクリップさせる
+			}
+			return;
+		}
+
+		// レイの方向に行進
+		pos += distance * rayDir;
+		totalDistance += distance;
+	}
+	
+	// 結果の表示
+	if (_DebugViewEnable) {
+		const float marchRate = (float)marchCount / _MaxMarchCount;
+		if (hit) {
+			BaseColor = float3(0, marchRate, 0); // ヒットした場合は緑色で回数表示
+		} else {
+			BaseColor = float3(marchRate, 0, 0); // 最後までヒットしなかった場合は赤色で回数表示
+		}
+	} else {
+		if (hit) {
+			// レイがヒットした場合
+			Normal = getNormal(pos);
+			DepthOffset = (mul(ViewMatrix, float4(FragmentPosition.xyz, 1)).z - mul(ViewMatrix, float4(pos.xyz, 1)).z) * sceneDepthDistanceRate;
+		} else {
+			// レイがヒットしなかった場合
+			Alpha = 0; // アルファを0にしてクリップさせる
+		}
+	}
+
+	return;
+}
+
+
+#endif
--- Assets/Mazkarisk/Metaball/MetaballController.cs
+++ Assets/Mazkarisk/Metaball/MetaballController.cs
@@ -2,8 +2,55 @@ using UnityEngine;
 
 [ExecuteAlways]
 public class MetaballController : MonoBehaviour {
+
+	private const int MAX_SPHERE_COUNT = 1024; // 球の最大個数（シェーダー側と合わせる）
+
+	[SerializeField]
+	private Material material = null;
+
+	[SerializeField]
+	float smoothWidth = 0.1f;
+	[SerializeField, Range(1, 256)]
+	int maxMarchCount = 256;
+	[SerializeField]
+	float hitThreshold = 0.001f;
+	[SerializeField]
+	bool debugViewEnable = false;
+
+	private readonly Vector4[] _spheres = new Vector4[MAX_SPHERE_COUNT];
+	ComputeBuffer spheresBuffer = null;
+
 	void Start() {
-		// フルスクリーン描画用ポリゴンを作成
+		GetComponent<MeshFilter>().sharedMesh = CreateMeshForFullScreenEffect();
+		GetComponent<MeshRenderer>().material = material;
+
+		// 球の設定
+		for (var i = 0; i < MAX_SPHERE_COUNT; i++) {
+			Vector2 randomInsideUnitCircle = Random.insideUnitCircle;
+			// 中心座標と半径を格納
+			_spheres[i] = new Vector4(randomInsideUnitCircle.x * 2f, Random.Range(0f, 1f), randomInsideUnitCircle.y * 2f, Random.Range(0.10f, 0.10f));
+		}
+
+		int stride = System.Runtime.InteropServices.Marshal.SizeOf(typeof(Vector4));
+		spheresBuffer = new ComputeBuffer(MAX_SPHERE_COUNT, stride, ComputeBufferType.Default);
+		spheresBuffer.SetData(_spheres);
+	}
+
+	void Update() {
+		material.SetInt("_SphereCount", 200);
+		material.SetFloat("_SmoothWidth", smoothWidth);
+		material.SetFloat("_MaxMarchCount", maxMarchCount);
+		material.SetFloat("_MaxMarchDistance", Camera.main.farClipPlane * 0.5f);
+		material.SetFloat("_HitThreshold", hitThreshold);
+		material.SetInt("_DebugViewEnable", debugViewEnable ? 1 : 0);
+		material.SetBuffer("_SpheresBuffer", spheresBuffer);
+	}
+
+	/// <summary>
+	/// フルスクリーンエフェクト用メッシュを新規作成する。
+	/// </summary>
+	/// <returns>新規作成されたフルスクリーンエフェクト用メッシュ</returns>
+	private Mesh CreateMeshForFullScreenEffect() {
 		Vector3[] vertices = new Vector3[4];
 		vertices[0] = new Vector3(-1, -1, 0);
 		vertices[1] = new Vector3(1, -1, 0);
@@ -26,10 +73,6 @@ public class MetaballController : MonoBehaviour {
 		mesh.RecalculateNormals();
 		mesh.RecalculateTangents();
 
-		GetComponent<MeshFilter>().sharedMesh = mesh;
-	}
-
-	void Update() {
-
+		return mesh;
 	}
 }
