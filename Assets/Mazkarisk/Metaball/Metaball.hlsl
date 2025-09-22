a7ec64ddd615bcd6763ae736e1a15e408186cd5c
mazkarisk <108807611+mazkarisk@users.noreply.github.com>
2025-09-22 18:29:01 +0900

    レイマーチを二段構えにして高速化、および物理演算の導入

--- Assets/Mazkarisk/Metaball/Metaball.hlsl
+++ Assets/Mazkarisk/Metaball/Metaball.hlsl
@@ -5,7 +5,8 @@
 /* 定数 */
 /********/
 
-#define MAX_SPHERE_COUNT 1024 // 球の最大数
+#define MAX_SPHERE_COUNT 65536 // 球の最大数
+#define MAX_STAKEHOLDERS_COUNT 64	// 単一Fragmentのレイマーチングの対象となる球の最大数
 
 
 /******************************/
@@ -14,10 +15,11 @@
 
 int _SphereCount; // 処理対象となる球の個数
 float _SmoothWidth; // 滑らかなmin関数の滑らか度合い
+float _MaxStakeholdableDistance; // 球をレイマーチング対象とする球表面⇔レイ間の最大の距離
 int _MaxMarchCount; // レイの最大行進回数
 float _MaxMarchDistance; // レイの最大行進距離(スクリプトから"カメラから最も遠い球の向こう側までの距離"を設定することを想定)
 float _HitThreshold; // レイの衝突判定閾値(レイの先端とメタボールの距離がこの値以下になったら"ヒット"したと判定する)
-int _DebugViewEnable; // デバッグ表示(0:しない, 1:更新回数を表示)
+int _DebugViewEnabled; // デバッグ表示(0:しない, 1:更新回数を表示)
 StructuredBuffer<float4> _SpheresBuffer; // 球の位置情報(x:x座標, y:y座標, z:z座標, w:半径)
 
 
@@ -37,20 +39,20 @@ float sphereDistanceFunction(float4 sphere, float3 pos) {
 }
 
 // 球との最短距離を返す
-float getDistance(float3 pos) {
+float getDistance(float3 pos, int stakeholdersIndices[MAX_STAKEHOLDERS_COUNT], int stakeholdersCount) {
 	float distance = _MaxMarchDistance; // 初期値として十分に遠い距離を設定
-	for (int i = 0; i < _SphereCount; i++) {
-		distance = smoothMin(distance, sphereDistanceFunction(_SpheresBuffer[i], pos), _SmoothWidth);
+	for (int i = 0; i < stakeholdersCount; i++) {
+		distance = smoothMin(distance, sphereDistanceFunction(_SpheresBuffer[stakeholdersIndices[i]], pos), _SmoothWidth);
 	}
 	return distance;
 }
 
 // 指定位置の法線を偏微分により取得
-float3 getNormal(const float3 pos) {
+float3 getNormal(const float3 pos, int stakeholdersIndices[MAX_STAKEHOLDERS_COUNT], int stakeholdersCount) {
 	const float delta = _HitThreshold;
-	const float x = getDistance(pos + float3(delta, 0, 0)) - getDistance(pos + float3(-delta, 0, 0));
-	const float y = getDistance(pos + float3(0, delta, 0)) - getDistance(pos + float3(0, -delta, 0));
-	const float z = getDistance(pos + float3(0, 0, delta)) - getDistance(pos + float3(0, 0, -delta));
+	const float x = getDistance(pos + float3(delta, 0, 0), stakeholdersIndices, stakeholdersCount) - getDistance(pos + float3(-delta, 0, 0), stakeholdersIndices, stakeholdersCount);
+	const float y = getDistance(pos + float3(0, delta, 0), stakeholdersIndices, stakeholdersCount) - getDistance(pos + float3(0, -delta, 0), stakeholdersIndices, stakeholdersCount);
+	const float z = getDistance(pos + float3(0, 0, delta), stakeholdersIndices, stakeholdersCount) - getDistance(pos + float3(0, 0, -delta), stakeholdersIndices, stakeholdersCount);
 	return normalize(float3(x, y, z));
 }
 
@@ -76,11 +78,42 @@ void Metaball_float(
 	const float sceneDepthDistanceRate = 1 / dot(rayDir, CameraDirection);
 	const float sceneDepthDistance = SceneDepth * sceneDepthDistanceRate;
 
-	// デフォルト値の設定
+	// デフォルト出力値の設定
 	BaseColor = float3(1, 1, 1); // 色は白
 	Alpha = 1; // アルファクリッピングは無し
 	Normal = float3(0, 1, 0); // 法線は上向き
 	DepthOffset = 0; // 深度オフセットは無し
+
+	// このFragmentでレイマーチングの対象とする球(のindex)を事前にリストアップしておく
+	int stakeholdersCount = 0; // このFragmentでレイマーチングの対象とする球の数
+	int stakeholdersIndices[MAX_STAKEHOLDERS_COUNT]; // indexを格納する配列
+	bool isStakeholdersOverflowed = false;
+	for (int i = 0; i < _SphereCount; i++) {
+		const float4 sphere = _SpheresBuffer[i];
+		const float t = dot(rayDir, (sphere.xyz - CameraPosition)); // カメラ位置からレイと球の最近傍点までの長さ
+		const float3 h = CameraPosition + rayDir * t; // レイと球の最近傍点の座標
+		
+		// レイと球の距離が_MaxStakeholdableDistance以下なら、球のindexを配列に格納する
+		if (sphereDistanceFunction(sphere, h) <= _MaxStakeholdableDistance) {
+			// 配列に入りきらなければエラーフラグを立てる
+			if (MAX_STAKEHOLDERS_COUNT <= stakeholdersCount) {
+				isStakeholdersOverflowed = true;
+				break;
+			}
+			stakeholdersIndices[stakeholdersCount] = i;
+			stakeholdersCount++;
+		}
+	}
+	
+	// 配列が空なら処理終了
+	if (stakeholdersCount <= 0) {
+		if (_DebugViewEnabled) {
+			BaseColor = float3(0, 0, 0); // 黒一色を表示
+		} else {
+			Alpha = 0; // アルファを0にしてクリップさせる
+		}
+		return;
+	}
 
 	// レイマーチング準備
 	float3 pos = CameraPosition; // レイの座標(カメラのワールド座標から開始)
@@ -90,7 +123,7 @@ void Metaball_float(
 	
 	// レイマーチング開始
 	for (marchCount = 1; marchCount <= _MaxMarchCount; marchCount++) {
-		const float distance = getDistance(pos);
+		const float distance = getDistance(pos, stakeholdersIndices, stakeholdersCount);
 
 		// 距離が閾値以下になったらヒットしたとみなして処理終了
 		if (distance <= _HitThreshold) {
@@ -100,7 +133,7 @@ void Metaball_float(
 
 		// 合計行進距離が描画済み深度に到達したら強制終了
 		if (totalDistance >= sceneDepthDistance) {
-			if (_DebugViewEnable) {
+			if (_DebugViewEnabled) {
 				const float marchRate = (float)marchCount / _MaxMarchCount;
 				BaseColor = float3(0, 0, marchRate); // 青色で回数を表示
 			} else {
@@ -111,7 +144,7 @@ void Metaball_float(
 
 		// 合計行進距離が最大描画距離に到達したら強制終了
 		if (totalDistance >= _MaxMarchDistance) {
-			if (_DebugViewEnable) {
+			if (_DebugViewEnabled) {
 				const float marchRate = (float)marchCount / _MaxMarchCount;
 				BaseColor = float3(0, marchRate, marchRate); // 水色で回数を表示
 			} else {
@@ -126,17 +159,17 @@ void Metaball_float(
 	}
 	
 	// 結果の表示
-	if (_DebugViewEnable) {
+	if (_DebugViewEnabled) {
 		const float marchRate = (float)marchCount / _MaxMarchCount;
-		if (hit) {
-			BaseColor = float3(0, marchRate, 0); // ヒットした場合は緑色で回数表示
+		if (hit && !isStakeholdersOverflowed) {
+			BaseColor = float3(0, marchRate, 0); // 正常にヒットした場合は緑色で回数表示
 		} else {
-			BaseColor = float3(marchRate, 0, 0); // 最後までヒットしなかった場合は赤色で回数表示
+			BaseColor = float3(marchRate, 0, 0); // 最後までヒットしなかった、あるいは配列があふれていた場合は赤色で回数表示
 		}
 	} else {
 		if (hit) {
 			// レイがヒットした場合
-			Normal = getNormal(pos);
+			Normal = getNormal(pos, stakeholdersIndices, stakeholdersCount);
 			DepthOffset = (mul(ViewMatrix, float4(FragmentPosition.xyz, 1)).z - mul(ViewMatrix, float4(pos.xyz, 1)).z) * sceneDepthDistanceRate;
 		} else {
 			// レイがヒットしなかった場合
--- Assets/Mazkarisk/Metaball/MetaballController.cs
+++ Assets/Mazkarisk/Metaball/MetaballController.cs
@@ -1,49 +1,103 @@
 using UnityEngine;
 
-[ExecuteAlways]
 public class MetaballController : MonoBehaviour {
 
-	private const int MAX_SPHERE_COUNT = 1024; // 球の最大個数（シェーダー側と合わせる）
+	private const int MAX_SPHERE_COUNT = 65536; // 球の最大個数（シェーダー側と合わせる）
 
 	[SerializeField]
 	private Material material = null;
 
 	[SerializeField]
 	float smoothWidth = 0.1f;
+	[SerializeField]
+	float maxStakeholdableDistance = 0.2f;
 	[SerializeField, Range(1, 256)]
 	int maxMarchCount = 256;
 	[SerializeField]
 	float hitThreshold = 0.001f;
 	[SerializeField]
-	bool debugViewEnable = false;
+	bool debugViewEnabled = false;
 
 	private readonly Vector4[] _spheres = new Vector4[MAX_SPHERE_COUNT];
 	ComputeBuffer spheresBuffer = null;
+
+	int sphereCount = 1024;
+	GameObject[] sphereObjects = new GameObject[MAX_SPHERE_COUNT];
+	Rigidbody[] sphereRigidbodies = new Rigidbody[MAX_SPHERE_COUNT];
+	SphereCollider[] sphereColliders = new SphereCollider[MAX_SPHERE_COUNT];
 
 	void Start() {
 		GetComponent<MeshFilter>().sharedMesh = CreateMeshForFullScreenEffect();
 		GetComponent<MeshRenderer>().material = material;
 
 		// 球の設定
-		for (var i = 0; i < MAX_SPHERE_COUNT; i++) {
-			Vector2 randomInsideUnitCircle = Random.insideUnitCircle;
-			// 中心座標と半径を格納
-			_spheres[i] = new Vector4(randomInsideUnitCircle.x * 2f, Random.Range(0f, 1f), randomInsideUnitCircle.y * 2f, Random.Range(0.10f, 0.10f));
+		for (var i = 0; i < sphereCount; i++) {
+			float radius = 0.02f;
+
+			sphereObjects[i] = new GameObject("MetaballSphere" + i);
+			sphereObjects[i].transform.parent = transform;
+			sphereRigidbodies[i] = sphereObjects[i].AddComponent<Rigidbody>();
+			sphereColliders[i] = sphereObjects[i].AddComponent<SphereCollider>();
+			sphereColliders[i].radius = radius * 0.5f; // 当たり判定の半径は半分にしておく
+
+			RespawnBall(i);
 		}
 
 		int stride = System.Runtime.InteropServices.Marshal.SizeOf(typeof(Vector4));
 		spheresBuffer = new ComputeBuffer(MAX_SPHERE_COUNT, stride, ComputeBufferType.Default);
-		spheresBuffer.SetData(_spheres);
 	}
 
 	void Update() {
-		material.SetInt("_SphereCount", 200);
+		for (var i = 0; i < sphereCount; i++) {
+			// 中心座標と半径を格納
+			Vector3 position = sphereRigidbodies[i].position;
+			float radius = sphereColliders[i].radius * 2f;
+			_spheres[i] = new Vector4(position.x, position.y, position.z, radius);
+		}
+		spheresBuffer.SetData(_spheres);
+
+		material.SetInt("_SphereCount", sphereCount);
 		material.SetFloat("_SmoothWidth", smoothWidth);
+		material.SetFloat("_MaxStakeholdableDistance", maxStakeholdableDistance);
 		material.SetFloat("_MaxMarchCount", maxMarchCount);
 		material.SetFloat("_MaxMarchDistance", Camera.main.farClipPlane * 0.5f);
 		material.SetFloat("_HitThreshold", hitThreshold);
-		material.SetInt("_DebugViewEnable", debugViewEnable ? 1 : 0);
+		material.SetInt("_DebugViewEnabled", debugViewEnabled ? 1 : 0);
 		material.SetBuffer("_SpheresBuffer", spheresBuffer);
+	}
+
+	void FixedUpdate() {
+		System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+		logText = "";
+
+		// リスポーン処理と最大速度の調査
+		sw.Restart();
+		float maxVelocity = 0f;
+		for (var i = 0; i < sphereCount; i++) {
+			if (sphereRigidbodies[i].linearVelocity.magnitude > maxVelocity) {
+				maxVelocity = sphereRigidbodies[i].linearVelocity.magnitude;
+			}
+
+			// 落ちたボールをリスポーン
+			if (sphereRigidbodies[i].position.y < -10) {
+				RespawnBall(i);
+			}
+		}
+		sw.Stop();
+		logText += "リスポーン処理の時間：" + sw.Elapsed.TotalMilliseconds + " ms\n";
+	}
+
+	private string logText = "";
+	private void OnGUI() {
+		// ログのテキストをスタイルに設定
+		GUIStyle guiStyleBack = new GUIStyle();
+		guiStyleBack.fontSize = 20;
+		guiStyleBack.normal.textColor = Color.black;
+		GUIStyle guiStyleFront = new GUIStyle();
+		guiStyleFront.fontSize = 20;
+		guiStyleFront.normal.textColor = Color.white;
+		GUI.Label(new Rect(12, 12, Screen.width, Screen.height), logText, guiStyleBack);
+		GUI.Label(new Rect(10, 10, Screen.width, Screen.height), logText, guiStyleFront);
 	}
 
 	/// <summary>
@@ -74,5 +128,17 @@ public class MetaballController : MonoBehaviour {
 		mesh.RecalculateTangents();
 
 		return mesh;
+	}
+
+	void RespawnBall(int index) {
+		Vector2 insideUnitCircle = Random.insideUnitCircle;
+
+		Vector3 position = new Vector3(insideUnitCircle.x, Random.Range(10f, 50f), insideUnitCircle.y);
+		Quaternion rotation = Random.rotation;
+
+		sphereObjects[index].transform.SetLocalPositionAndRotation(position, rotation);
+		sphereRigidbodies[index].linearDamping = 1.5f;  // FixedTime設定にもよるが、終端速度は6.3m/sくらいになる
+		sphereRigidbodies[index].linearVelocity = Vector3.down * 6.3f;
+		sphereRigidbodies[index].angularVelocity = Vector3.zero;
 	}
 }
